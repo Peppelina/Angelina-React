6b45b2f09fad3d4e01689585b211921391a4cd6e
Peppelina <proshekalnikovaa@gmail.com>
2022-02-05 20:47:31 +0300

    new navbar-i

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,9 @@ const Navbar = () => {
         <div><a href='#'>News</a></div>
         <div><a href='#'>Music</a></div>
         <div><a href='#'>Settings</a></div>
+        <div><a href='#'>I</a></div>
+        
+
     </nav>
 }
 
