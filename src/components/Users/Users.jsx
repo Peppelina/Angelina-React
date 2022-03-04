0ed7ca842332a85d9a0be80797e99316d8ea4236
Peppelina <proshekalnikovaa@gmail.com>
2022-03-04 15:42:54 +0300

    add class component users lesson 53-54

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,27 +3,23 @@ import classes from './Users.module.css'
 import UserItem from "./UserItem/UserItem";
 import axios from "axios";
 
+class Users extends React.Component {
 
-/*[{id: 1, photoUrl:'https://klike.net/uploads/posts/2018-10/1539499416_1.jpg', followed: false, fullName: 'Angelina', status: 'Im sing', location: {city: 'Kazan', country: 'Russia'}},
-    {id: 2, photoUrl: 'https://klike.net/uploads/posts/2019-07/medium/1564314059_1.jpg', followed: true, fulName: 'Viktoria', status: 'Im dance', location: {city: 'Moscow', country: 'Russia'}},
-    {id: 3, photoUrl:'https://klike.net/uploads/posts/2019-07/medium/1564314087_2.jpg', followed: false, fullName: 'Vadim', status: 'Im programmist', location: {city: 'Mendeleevsk', country: 'Russia'}},
-    {id: 4, photoUrl:'https://klike.net/uploads/posts/2019-07/medium/1564314090_3.jpg', followed: true, fullName: 'Anton', status: 'Im work', location: {city: 'Tolyatti', country: 'Russia'}},
-]*/
-const Users = (props) => {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => // данные заггруюжаются в стейт
+            this.props.setUsers (response.data.items)
+        )
+    }
 
-        if (props.users.length===0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => // данные заггруюжаются в стейт
-                props.setUsers (response.data.items)
-            )
-        }
+    render() {
+        let usersElements = this.props.users.map(user => <UserItem user={user} follow={this.props.follow} unfollow={this.props.unfollow}/>) //данные берем из стейта
 
-    let usersElements = props.users.map(user => <UserItem user={user} follow={props.follow} unfollow={props.unfollow}/>) //данные берем из стейта
-
-    return (
-        <div className={classes.wrapperUsers}>
-            {usersElements}
-        </div>
-    )
+        return (
+            <div className={classes.wrapperUsers}>
+                {usersElements}
+            </div>
+        )
+    }
 }
 
 export default Users
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -5,6 +5,11 @@ const SET_USERS = 'SET_USERS';
 let initialState = {
     users: [],
 }
+/*[{id: 1, photoUrl:'https://klike.net/uploads/posts/2018-10/1539499416_1.jpg', followed: false, fullName: 'Angelina', status: 'Im sing', location: {city: 'Kazan', country: 'Russia'}},
+    {id: 2, photoUrl: 'https://klike.net/uploads/posts/2019-07/medium/1564314059_1.jpg', followed: true, fulName: 'Viktoria', status: 'Im dance', location: {city: 'Moscow', country: 'Russia'}},
+    {id: 3, photoUrl:'https://klike.net/uploads/posts/2019-07/medium/1564314087_2.jpg', followed: false, fullName: 'Vadim', status: 'Im programmist', location: {city: 'Mendeleevsk', country: 'Russia'}},
+    {id: 4, photoUrl:'https://klike.net/uploads/posts/2019-07/medium/1564314090_3.jpg', followed: true, fullName: 'Anton', status: 'Im work', location: {city: 'Tolyatti', country: 'Russia'}},
+]*/
 
 const usersReducer = (state = initialState, action) => {
 
